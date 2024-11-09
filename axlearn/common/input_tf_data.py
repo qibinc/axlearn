112210db8075dd71debf1f721a1082a78d5cc213
Qibin Chen <qchen35@apple.com>
2024-11-09 03:15:10 -0800

    update

--- axlearn/common/input_tf_data.py
+++ axlearn/common/input_tf_data.py
@@ -381,7 +381,7 @@ def sample_from_datasets(
     sources: Sequence[InstantiableConfig],
     weights: Sequence[float],
     seed: Optional[int] = None,
-    autotune_ram_budget_gb: Optional[int] = 200,
+    autotune_ram_budget_gb: Optional[int] = 300,
 ) -> BuildDatasetFn:
     """Returns a data source formed by sampling from multiple data sources without replacement.
     All source datasets are repeated to prevent the sampling from stopping early and to prevent
